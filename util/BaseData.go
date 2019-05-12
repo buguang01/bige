f6618c5676c15b0718d6c46d616f975d56e26176
buguang01 <273033431@qq.com>
2019-05-12 16:42:00 +0800

    BaseData 改成struct，去掉SQLmodule的注释代码

--- util/BaseData.go
+++ util/BaseData.go
@@ -1,62 +1,72 @@
 package util
 
 //BaseData 基础仓库数据
-type BaseData map[int]int
+type BaseData struct {
+	Data map[int]int
+}
 
-func (this BaseData) Clone() BaseData {
-	result := make(BaseData)
-	for k, v := range this {
-		result[k] = v
+//Clone 复制一个BaseData
+func (this BaseData) Clone() *BaseData {
+	result := new(BaseData)
+	result.Data = make(map[int]int)
+	for k, v := range this.Data {
+		result.Data[k] = v
 	}
 	return result
 }
 
-func NewBaseDataString(str string) BaseData {
-	result := make(BaseData)
+//NewBaseDataString 用字符串初始化一个数据
+func NewBaseDataString(str string) *BaseData {
+	result := new(BaseData)
+	result.Data = make(map[int]int)
 	if str == "" || str == "0" {
 		return result
 	}
 	arr := StringToIntArray(str, ";")
 	for i := 0; i < len(arr); i += 2 {
-		result[arr[i]] = arr[i+1]
+		result.Data[arr[i]] = arr[i+1]
 	}
 	return result
 }
 
-func (this BaseData) UpData(key, num int) {
-	v, _ := this[key]
+//UpData 更新指定数据
+func (this *BaseData) UpData(key, num int) {
+	v, _ := this.Data[key]
 	if num+v > 0 {
-		this[key] = v + num
+		this.Data[key] = v + num
 	} else {
-		delete(this, key)
+		delete(this.Data, key)
 	}
 }
 
-func (this BaseData) UpDataBc(addbc, delbc BaseData) {
+//UpDataBc批量用别的数据，更新本数据
+func (this *BaseData) UpDataBc(addbc, delbc *BaseData) {
 	if delbc != nil {
-		for k, n := range delbc {
+		for k, n := range delbc.Data {
 			this.UpData(k, -n)
 		}
 	}
 	if addbc != nil {
-		for k, n := range addbc {
+		for k, n := range addbc.Data {
 			this.UpData(k, n)
 		}
 	}
 }
 
-func (this BaseData) GetNumByKey(key int) int {
-	v, ok := this[key]
+//GetNumByKey指定数据的值
+func (this *BaseData) GetNumByKey(key int) int {
+	v, ok := this.Data[key]
 	if !ok {
 		return 0
 	}
 	return v
 }
 
-func (this BaseData) ToString() string {
+//ToString 字符串化
+func (this *BaseData) ToString() string {
 	sb := NewStringBuilder()
 	t := 0
-	for k, v := range this {
+	for k, v := range this.Data {
 		if t == 0 {
 			t++
 		} else {
@@ -70,16 +80,16 @@ func (this BaseData) ToString() string {
 }
 
 //Count 总数量
-func (this BaseData) Count() (result int) {
-	for _, n := range this {
+func (this *BaseData) Count() (result int) {
+	for _, n := range this.Data {
 		result += n
 	}
 	return result
 }
 
 //MaxItem 最大数值的KEY，value
-func (this BaseData) MaxItem() (key, num int) {
-	for k, n := range this {
+func (this *BaseData) MaxItem() (key, num int) {
+	for k, n := range this.Data {
 		if n > num {
 			key, num = k, n
 		}
