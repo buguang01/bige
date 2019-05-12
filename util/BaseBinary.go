2af4762cd9f626dc596fe7735f4f66372051ce16
buguang01 <273033431@qq.com>
2019-05-12 15:49:03 +0800

    mysql 的binary的数据类型操作对象

---
+++ util/BaseBinary.go
@@ -0,0 +1,90 @@
+package util
+
+//BaseBinary DB上的2进制数据
+type BaseBinary struct {
+	Data          []uint8 //放数据的数组
+	ArrayLen      int     //上面那个数组的长度
+	OneDataBitNum uint8   //一个数据占多少个位，只能是1，2，4，8
+}
+
+func NewBinary(alen int) *BaseBinary {
+	result := new(BaseBinary)
+	result.Data = make([]uint8, alen)
+	result.ArrayLen = alen
+	result.OneDataBitNum = 1
+	return result
+}
+func NewBinaryByLen(alen, dlen int) *BaseBinary {
+	result := new(BaseBinary)
+
+	result.Data = make([]uint8, alen)
+	result.ArrayLen = alen
+	result.OneDataBitNum = uint8(dlen)
+	return result
+}
+
+func (this *BaseBinary) Init(d []uint8) {
+	this.Data = d
+	this.ArrayLen = len(this.Data)
+}
+
+func (this *BaseBinary) ContainKey(index int) int {
+	dlen := int(this.OneDataBitNum)
+	ai := index / (8 / dlen)
+	bitnum := uint8(index % (8 / dlen) * dlen)
+	if ai >= this.ArrayLen {
+		return 0
+	}
+
+	d := this.Data[ai]
+	if this.OneDataBitNum == 8 {
+		return int(d)
+	}
+	var mask uint8 = (1 << this.OneDataBitNum) - 1
+	return int((d & (mask << bitnum)) >> bitnum)
+}
+
+func (this *BaseBinary) UpData(index, val int) bool {
+	bval := uint8(val)
+
+	dlen := int(this.OneDataBitNum)
+	ai := (index / (8 / dlen))
+	bitnum := uint8(index % (8 / dlen) * dlen)
+	key := this.Data[ai]
+	if ai >= this.ArrayLen {
+		return false
+	}
+	if this.OneDataBitNum == 8 {
+		key = bval
+	} else {
+		var tmp uint8 = (1 << this.OneDataBitNum) - 1
+		tmp = tmp << bitnum
+		tmp = key & ^tmp
+		key = uint8(tmp + (bval << bitnum))
+	}
+	this.Data[ai] = key
+	return true
+
+}
+
+func (this *BaseBinary) ToValuesJson() []interface{} {
+	dlen := int(this.OneDataBitNum)
+	result := make([]interface{}, this.ArrayLen*dlen/8)
+	index := 0
+	for k := 0; k < this.ArrayLen; k++ {
+		key := this.Data[k]
+		if this.OneDataBitNum == 8 {
+			result[index] = key
+			index++
+		} else {
+
+			for bitnum := uint8(0); bitnum < 8; bitnum = bitnum + this.OneDataBitNum {
+				var mask uint8 = (1 << this.OneDataBitNum) - 1
+				result[index] = (key & (mask << bitnum)) >> bitnum
+				index++
+			}
+		}
+
+	}
+	return result
+}
