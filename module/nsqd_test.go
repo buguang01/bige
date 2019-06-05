0cc566518018020fa389fd4d4ee3bf94d12e0c7f
buguang01 <273033431@qq.com>
2019-06-05 20:00:44 +0800

    nsqd test

---
+++ module/nsqd_test.go
@@ -0,0 +1,26 @@
+package module_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/buguang01/Logger"
+
+	"github.com/buguang01/gsframe/module"
+)
+
+func TestNsqd(t *testing.T) {
+	Logger.Init(1, "logs")
+
+	nsqd := module.NewNsqdModule(&module.NsqdConfig{
+		Addr:                "192.168.39.97:4150",
+		NSQLookupdAddr:      "192.168.39.97:4161",
+		ChanNum:             100,
+		LookupdPollInterval: 1000,
+		MaxInFlight:         1000,
+	}, 4)
+	nsqd.Init()
+	nsqd.Start()
+
+	time.Sleep(100 * time.Second)
+}
